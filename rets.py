05b4801652e3f52999b3777973d13456d407e4ec
Theodoros <thikonom@gmail.com>
2013-07-28 18:17:19 +0100

    move assertions before try

--- rets.py
+++ rets.py
@@ -3,11 +3,12 @@ import requests
 class Rets(object):
 
     def login(self, login_url, username, password):
+        assert login_url != '', 'Login url is empty'
+        assert username  != '', 'Username is empty'
+        assert password  != '', 'Password is empty'
+
         response = None
         try:
-            assert login_url != '', 'Login url is empty'
-            assert username  != '', 'Username is empty'
-            assert password  != '', 'Password is empty'
 
             response = requests.get(url=login_url, auth=(username, password)) 
         except Exception as e:
