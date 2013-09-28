d82b06395d5c230d3a1796deb15da3baceaca45e
Theodoros Ikonomou <thikonom@gmail.com>
2013-09-28 01:12:21 +0100

    - add login_required decorator - add a persistant session

--- rets/rets.py
+++ rets/rets.py
@@ -1,13 +1,24 @@
 from urlparse import urlparse
 from urllib import urlencode
 from StringIO import StringIO
+import copy
 
 from lxml import etree
 import requests
+from requests.auth import HTTPDigestAuth, HTTPBasicAuth
 
 from .exceptions import RetsException
-from .utils import assert_successful_response
+from .utils import assert_successful_response, login_required
 
+
+_last_response_cache = {}
+
+def clear_cache():
+    _last_response_cache.clear()
+
+UA_AUTH_TYPE = ['USER_AGENT_AUTH_RETS_1_7', # RETS 1.7 user agent authorization.
+                'USER_AGENT_AUTH_INTEREALTY' # The Interealty variant of 1.7 user agent authorization.
+               ]
 
 class Rets(object):
 
@@ -29,6 +40,10 @@ class Rets(object):
     default_headers = {'RETS-Version': 'RETS/1.5',
                        'User-Agent':   'Rets-py/1.0'}
 
+    use_basic_authentication = False
+    ua_passwd = ""
+    ua_auth_type = UA_AUTH_TYPE[0]
+
     def __init__(self, **kwargs):
         self.capability_urls = {}
         self.server_info = {}
@@ -39,8 +54,11 @@ class Rets(object):
             self.headers.update(headers)
         self.__logged_in = False
 
-    def login(self, login_url, username, password):
-        assert login_url != '', 'Login url cannot be empty'
+    def login(self, login_url, username, password, ua_passwd=''):
+        self.__logged_in = False
+        self.session = None
+
+        assert login_url != '',  'Login url cannot be empty'
         assert username != '',   'Username cannot be empty'
         assert password != '',   'Password cannot be empty'
 
@@ -53,6 +71,22 @@ class Rets(object):
         self.password = password
 
         action = uri_portions.path
+        
+        base_req = requests.Request(method='GET', 
+                                   headers=self.headers)
+                             
+        if self.use_basic_authentication:
+            # try only basic authentication
+            ba_req = copy.deepcopy(base_req)
+            self.reqs = [ba_req]
+        else:
+            # try both basic and digest authentication
+            ba_req = copy.deepcopy(base_req)
+            ba_req.auth = HTTPBasicAuth(self.username, self.password)
+            dg_req = copy.deepcopy(base_req)
+            dg_req.auth = HTTPDigestAuth(self.username, self.password)
+            self.reqs = [ba_req, dg_req]
+
         response = self.dorequest(action)
 
         if response.status_code == 401:
@@ -61,6 +95,20 @@ class Rets(object):
                 return False
 
         assert_successful_response(response, login_url)
+
+        _last_response_cache.update(response.headers)
+
+        # update to last response header
+        detected_rets_version = _last_response_cache.get('rets-version')
+        if detected_rets_version:
+            self.headers['RETS-Version'] = detected_rets_version
+
+        auth_support = _last_response_cache.get('www-authenticate')
+        if auth_support:
+            if 'Basic' in auth_support: 
+                self.auth_support_basic = True
+            if 'Digest' in auth_support:
+                self.auth_support_digest = True
 
         self._set_capability_urls(response)
 
@@ -90,9 +138,10 @@ class Rets(object):
             else:
                 self.server_info[key] = value
 
-    def dorequest(self, action, *args):
+    def dorequest(self, action, **kwargs):
         assert action != '', 'Action cannot be empty'
 
+        # TODO set cookie if ua_passwd is not empty
         uri_portions = urlparse(action)
         if not uri_portions.netloc:
             # action is provided as a relative path
@@ -103,11 +152,58 @@ class Rets(object):
             request_url = action
 
         request_args = ''
-        if args:
-            request_args = urlencode(args)
+        if kwargs:
+            request_args = urlencode(kwargs)
+            request_url = "?".join([request_url, request_args])
 
-        response = requests.get(url=request_url,
-                                auth=(self.username, self.password),
-                                headers=self.headers)
+        for req in self.reqs:
+            req.url = request_url
+            r = req.prepare()
+            if self.session is None:
+                # if there is no existing session
+                self.session = requests.Session()
+            response = self.session.send(r)
+            if response.status_code == 200: 
+                break
 
         return response
+
+    @property
+    def is_logged_in(self):
+        return self.__logged_in
+
+    @login_required
+    def get_server_information(self):
+        metadata_url = self.capability_urls.get('GETMETADATA')
+        assert metadata_url != '', 'No url for capability GetMetadata found'
+
+        response = self.dorequest(metadata_url, **{'Type': 'METADATA-SYSTEM',
+                                                    'ID': '0',
+                                                    'Format': 'STANDARD-XML'})
+        return response
+
+    #def get_metadata(self, resource, klass):
+    #    if not self.__logged_in:
+    #        raise RetsException('You are not logged in')
+
+    #    metadata_url = self.capability_urls.get('GETMETADATA')
+    #    assert metadata_url, 'No url for capability GetMetadata found'
+
+    #    metadata_url += urlencode({'Type': 'METADATA-LOOKUP_TYPE',
+    #                               'ID': '',
+    #                               'Format': 'STANDARD-XML'})
+    #    return self.get_metadata_table(resource, klass)
+
+    #def get_metadata_table(self, resource, klass):
+
+    #    assert resource, 'Resource parameter is required in get_metadata() request'
+    #    assert klass, 'Class parameter is required in get_metadata() request'
+
+    #    metadata_url = self.capability_urls['GETMETADATA']
+    #    metadata_url += urlencode({'Type': 'METADATA-LOOKUP_TYPE',
+    #                               'ID': '',
+    #                               'Format': 'STANDARD-XML'})
+
+    #    try:
+    #        response = requests.get(url=metadata_url, auth=(self.username, self.password), header=self.headers)
+    #    except Exception as e:
--- rets/utils.py
+++ rets/utils.py
@@ -1,4 +1,14 @@
+from functools import wraps
+
 from .exceptions import RetsHTTPException
+
+def login_required(fn):
+    @wraps(fn)
+    def _inner(*args, **kwargs):
+        if args[0].is_logged_in:
+            return fn(*args, **kwargs)
+        raise RetsException('You are not logged in')
+    return _inner
 
 def assert_successful_response(response, url):
     response_code = response.status_code
