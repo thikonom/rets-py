aa43fa2b345b2ad42f210059d7bdc8cc756df474
Theodoros Ikonomou <thikonom@gmail.com>
2013-11-05 18:56:16 +0000

    replace httpretty with mock library

--- test_rets.py
+++ test_rets.py
@@ -1,9 +1,14 @@
-import rets
-from rets.exceptions import RetsHTTPException
-
 import unittest
 
-import httpretty
+from mock import Mock, patch
+
+import rets
+from rets.exceptions import RetsHTTPException
+from rets import Rets
+
+
+class FakeResponse(object):
+    """"FakeResponse class"""
 
 class RetsUnitTest(unittest.TestCase):
 
@@ -12,7 +17,7 @@ class RetsUnitTest(unittest.TestCase):
         self.password  = "1234"
 
     def test_successful_login(self):
-        mock_response = """
+        fake_response_text = """
             <RETS ReplyCode="0" ReplyText="Operation Successful">
             <RETS-RESPONSE>
             MemberName=Retspy
@@ -35,64 +40,70 @@ class RetsUnitTest(unittest.TestCase):
             </RETS>
             """
 
-        httpretty.enable()
-        httpretty.register_uri(httpretty.GET,'http://www.retserver.com/rets/login', body=mock_response)
+            
+        fakeobj = FakeResponse()
+        fakeobj.__dict__ = {
+                'text': fake_response_text,
+                'status_code': 200,
+                'headers': dict()
+        }
 
-        client = rets.Rets()
-        response = client.login('http://www.retserver.com/rets/login', self.username, self.password)
+        with patch.object(Rets, 'dorequest', return_value=fakeobj):
+            client = rets.Rets()
+            response = client.login('http://www.retserver.com/rets/login', self.username, self.password)
 
-        self.assertEqual(response, True)
-        self.assertEqual(client.server_info, {
-            'BROKER': 'NA',
-            'MEMBERNAME': 'Retspy',
-            'METADATATIMESTAMP': '2013-08-09T21:36:19',
-            'METADATAVERSION': '1.00.00041',
-            'MINMETADATATIMESTAMP': '2013-08-09T21:36:19',
-            'TIMEOUTSECONDS': '1800000',
-            'USER': 'Retspy,0,Syndicator,Retspy'
-        })
-        self.assertEqual(client.capability_urls, {
-           'ACTION': '/rets/action',
-           'GETMETADATA': '/rets/getMetadata',
-           'GETOBJECT': '/rets/getObject',
-           'LOGIN': '/rets/login',
-           'LOGOUT': '/rets/logout',
-           'SEARCH': '/rets/search',
-           'UPDATE': '/rets/update',
-           'X-POSTOBJECT': '/rets/postObject',
-           'X-SELECTOR': '/rets/selector'
-        })  
+            self.assertEqual(response, True)
+            self.assertEqual(client.server_info, {
+                'BROKER': 'NA',
+                'MEMBERNAME': 'Retspy',
+                'METADATATIMESTAMP': '2013-08-09T21:36:19',
+                'METADATAVERSION': '1.00.00041',
+                'MINMETADATATIMESTAMP': '2013-08-09T21:36:19',
+                'TIMEOUTSECONDS': '1800000',
+                'USER': 'Retspy,0,Syndicator,Retspy'
+            })
+            self.assertEqual(client.capability_urls, {
+               'ACTION': '/rets/action',
+               'GETMETADATA': '/rets/getMetadata',
+               'GETOBJECT': '/rets/getObject',
+               'LOGIN': '/rets/login',
+               'LOGOUT': '/rets/logout',
+               'SEARCH': '/rets/search',
+               'UPDATE': '/rets/update',
+               'X-POSTOBJECT': '/rets/postObject',
+               'X-SELECTOR': '/rets/selector'
+            })  
 
-        httpretty.disable()
 
     def test_unauthorized_login(self):
-        httpretty.enable()
-        httpretty.register_uri(httpretty.GET, "http://www.retserver.com/rets/login", 
-                               responses = [httpretty.Response(body='404 Not Found', status=401),
-                                            httpretty.Response(body='404 Not Found', status=401)])
+        fakeobj = FakeResponse()
+        fakeobj.__dict__ = {
+                'text': 'Unauthorized',
+                'status_code': 401,
+                'headers': dict()
+        }
 
-        client = rets.Rets()
-
-        response = client.login("http://www.retserver.com/rets/login", self.username, self.password)
-        self.assertEqual(response, False)
-        self.assertEqual(client.capability_urls, {})
-        self.assertEqual(client.server_info, {})
-
-        httpretty.disable()
-
-    def test_unsuccessful_login(self):
-        httpretty.enable()
-        httpretty.register_uri(httpretty.GET, "http://www.retserver.com/rets/login", status=404)
-
-        client = rets.Rets()
-
-        with self.assertRaises(RetsHTTPException):
-            client.login("http://www.retserver.com/rets/login", self.username, self.password)
+        with patch.object(Rets, 'dorequest', return_value=fakeobj):
+            client = rets.Rets()
+            response = client.login('http://www.retserver.com/rets/login', self.username, self.password)
+            self.assertEqual(response, False)
             self.assertEqual(client.capability_urls, {})
             self.assertEqual(client.server_info, {})
 
-        httpretty.disable()
 
+    def test_unsuccessful_login(self):
+        fakeobj = FakeResponse()
+        fakeobj.__dict__ = {
+                'text': '404: Not Found',
+                'status_code': 404,
+                'headers': dict()
+        }
+
+        with patch.object(Rets, 'dorequest', return_value=fakeobj), self.assertRaises(RetsHTTPException):
+            client = rets.Rets()
+            client.login("http://www.retserver.com/rets/login", self.username, self.password)
+            self.assertEqual(client.capability_urls, {})
+            self.assertEqual(client.server_info, {})
 
 
 if __name__ == '__main__':
