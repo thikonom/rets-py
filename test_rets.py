eff0f62d0f6c955138e07c3ff60423070c9e3638
Theodoros Ikonomou <thikonom@gmail.com>
2013-11-16 00:17:54 +0000

    get_system_metadata handles all rets server versions add tests for get_system_metadata add _pretend_login to pretend successful login

--- test_rets.py
+++ test_rets.py
@@ -16,6 +16,24 @@ class RetsUnitTest(unittest.TestCase):
         self.username  = "admin"
         self.password  = "1234"
 
+        self.client = rets.Rets()
+        self.fakeobj = FakeResponse()
+
+    def _pretend_login(self):
+        self.client._Rets__logged_in = True
+        self.client.capability_urls = {
+            'ACTION': '/rets/action',
+            'GETMETADATA': '/rets/getMetadata',
+            'GETOBJECT': '/rets/getObject',
+            'LOGIN': '/rets/login',
+            'LOGOUT': '/rets/logout',
+            'SEARCH': '/rets/search',
+            'UPDATE': '/rets/update',
+            'X-POSTOBJECT': '/rets/postObject',
+            'X-SELECTOR': '/rets/selector'
+        }
+
+
     def test_successful_login(self):
         fake_response_text = """
             <RETS ReplyCode="0" ReplyText="Operation Successful">
@@ -40,20 +58,18 @@ class RetsUnitTest(unittest.TestCase):
             </RETS>
             """
 
-            
-        fakeobj = FakeResponse()
-        fakeobj.__dict__ = {
-                'text': fake_response_text,
-                'status_code': 200,
-                'headers': dict()
+        self.fakeobj.__dict__ = {
+            'text': fake_response_text,
+            'status_code': 200,
+            'headers': dict()
         }
 
-        with patch.object(Rets, 'dorequest', return_value=fakeobj):
-            client = rets.Rets()
-            response = client.login('http://www.retserver.com/rets/login', self.username, self.password)
+        with patch.object(Rets, 'dorequest', return_value=self.fakeobj):
+            response = self.client.login('http://www.retserver.com/rets/login',
+                                         self.username, self.password)
 
             self.assertEqual(response, True)
-            self.assertEqual(client.server_info, {
+            self.assertEqual(self.client.server_info, {
                 'BROKER': 'NA',
                 'MEMBERNAME': 'Retspy',
                 'METADATATIMESTAMP': '2013-08-09T21:36:19',
@@ -62,7 +78,7 @@ class RetsUnitTest(unittest.TestCase):
                 'TIMEOUTSECONDS': '1800000',
                 'USER': 'Retspy,0,Syndicator,Retspy'
             })
-            self.assertEqual(client.capability_urls, {
+            self.assertEqual(self.client.capability_urls, {
                'ACTION': '/rets/action',
                'GETMETADATA': '/rets/getMetadata',
                'GETOBJECT': '/rets/getObject',
@@ -72,39 +88,67 @@ class RetsUnitTest(unittest.TestCase):
                'UPDATE': '/rets/update',
                'X-POSTOBJECT': '/rets/postObject',
                'X-SELECTOR': '/rets/selector'
-            })  
+            })
 
 
     def test_unauthorized_login(self):
-        fakeobj = FakeResponse()
-        fakeobj.__dict__ = {
-                'text': 'Unauthorized',
-                'status_code': 401,
-                'headers': dict()
+        self.fakeobj.__dict__ = {
+            'text': 'Unauthorized',
+            'status_code': 401,
+            'headers': dict()
         }
 
-        with patch.object(Rets, 'dorequest', return_value=fakeobj):
-            client = rets.Rets()
-            response = client.login('http://www.retserver.com/rets/login', self.username, self.password)
+        with patch.object(Rets, 'dorequest', return_value=self.fakeobj):
+            response = self.client.login('http://www.retserver.com/rets/login',
+                                         self.username, self.password)
             self.assertEqual(response, False)
-            self.assertEqual(client.capability_urls, {})
-            self.assertEqual(client.server_info, {})
+            self.assertEqual(self.client.capability_urls, {})
+            self.assertEqual(self.client.server_info, {})
 
 
     def test_unsuccessful_login(self):
-        fakeobj = FakeResponse()
-        fakeobj.__dict__ = {
-                'text': '404: Not Found',
-                'status_code': 404,
-                'headers': dict()
+        self.fakeobj.__dict__ = {
+            'text': '404: Not Found',
+            'status_code': 404,
+            'headers': dict()
         }
 
-        with patch.object(Rets, 'dorequest', return_value=fakeobj), self.assertRaises(RetsHTTPException):
-            client = rets.Rets()
-            client.login("http://www.retserver.com/rets/login", self.username, self.password)
-            self.assertEqual(client.capability_urls, {})
-            self.assertEqual(client.server_info, {})
+        with patch.object(Rets, 'dorequest', return_value=self.fakeobj), self.assertRaises(RetsHTTPException):
+            self.client.login("http://www.retserver.com/rets/login",
+                              self.username, self.password)
+            self.assertEqual(self.client.capability_urls, {})
+            self.assertEqual(self.client.server_info, {})
 
+
+    def test_get_system_metadata(self):
+
+        self._pretend_login()
+
+        fake_response_text = """
+            <RETS ReplyCode="0" ReplyText="Operation Successful">
+            <METADATA-SYSTEM Version="100.00.001" Date="Mon, 18 Aug 2003 17:00:00 GMT">
+            <SYSTEM SystemID="CRT_RETS" SystemDescription="Center for REALTOR Technology"/>
+            <COMMENTS>The reference implementation of a RETS Server</COMMENTS>
+            </METADATA-SYSTEM>
+            </RETS>
+            """
+
+        fake_system_metadata = {
+            'Version': '100.00.001',
+            'SystemID': 'CRT_RETS',
+            'SystemDescription': 'Center for REALTOR Technology',
+            'Comments': 'The reference implementation of a RETS Server',
+            'TimeZoneOffset': ''
+        }
+
+        self.fakeobj.__dict__ = {
+            'text': fake_response_text,
+            'status_code': 200,
+            'headers': dict()
+        }
+        with patch.object(Rets, 'dorequest', return_value=self.fakeobj):
+            self.assertEquals(self.client.get_system_metadata(),
+                              fake_system_metadata)
 
 if __name__ == '__main__':
     unittest.main()
